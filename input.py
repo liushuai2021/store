a29f639e66d5d1b1d049068131ceb3d98a5df36f
liushuai2021 <88364874+liushuai2021@users.noreply.github.com>
2021-08-10 17:26:53 +0800

    Delete input.py

--- input.py
+++
@@ -1,9 +0,0 @@
-#从键盘上输入个人信息（身份证编号，姓名，年龄，性别，身高，居住地址）并使用模板格式化打印出来。
-# （模板自己定义越好看越好！）
-id=input("请输入身份证号")
-name=input("请输入姓名")
-age=input("请输入性别")
-high=input("请输入身高")
-adress=input("请输入地址")
-print(id,name,age,high,adress)
-
